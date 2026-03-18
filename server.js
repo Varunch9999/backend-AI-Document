7a2fc002d67d2728c869e5abdcd4dc43cff4c3cf
Varunch9999 <varunnn.ch@gmail.com>
2026-03-18 14:26:26 +0530

    added cors

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({ 
+    origin: ["http://localhost:5173","https://frontend-ai-document.vercel.app"],
+    credentials: true 
+}));
 app.use(express.json());
 
 
